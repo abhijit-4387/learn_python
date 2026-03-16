580fd666e898f4d0997c0f97c43a9a27b8b92bac
abhijit 4387 <ccabhijithraghava@gmail.com>
2026-03-16 21:51:22 +0530

    Initial Commit

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+# As the first step of learning Python, we will write a simple program that prints "Hello, World!" to the console.
+
+print("Hello World!")
+
+# When you run this code, it will output:
+
+# Hello World!
